1434845dccd1f598649a3fa7162632b244b6333f
hamnaaqamar <153479234+hamnaaqamar@users.noreply.github.com>
2024-02-20 20:46:16 +0500

    Add files via upload

---
+++ main_page.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.markdown("# MULTI-TASKING AI PROJECT ")
+st.sidebar.markdown("# HOME")
+
+def main_page():
+    st.markdown("# MULTI-TASKING AI PROJECT")
+    st.sidebar.markdown("# HOME")
